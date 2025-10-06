8267c925d8d1a67897dc67f588eae7bc2ea27dcb
Dubugui <gabrielbcostadev@gmail.com>
2025-10-06 18:13:27 -0300

    app tava errado essa porra

--- GameLog/src/app/app.ts
+++ GameLog/src/app/app.ts
@@ -1,17 +1,14 @@
-// src/app/app.ts
-
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/header/header'; // <--- Importe aqui!
+import { HeaderComponent } from './shared/header/header'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  // Adicione HeaderComponent e RouterOutlet aos imports
   imports: [RouterOutlet, HeaderComponent], 
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
-export class AppComponent {
+export class App {
   title = 'GameLog';
 }
