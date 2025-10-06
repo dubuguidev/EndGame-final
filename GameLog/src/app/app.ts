3111159120a723e31ba1d5a9cf1e8afa588491f6
Dubugui <gabrielbcostadev@gmail.com>
2025-10-06 19:09:49 -0300

    Update app.ts

--- GameLog/src/app/app.ts
+++ GameLog/src/app/app.ts
@@ -1,25 +1,31 @@
+// src/app/app.ts (Versão Completa e Final)
+
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // <--- ONDE ESTÃO O *ngIf E | async
 import { HeaderComponent } from './shared/header/header'; 
 import { AuthService } from './core/auth.service';
-import { Observable } from 'rxjs'; // <--- PRECISA DE Observable
+import { Observable } from 'rxjs'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent], 
+  imports: [
+    CommonModule, // <--- ESTE ITEM RESOLVE O ERRO NG0302 DO | async
+    RouterOutlet, 
+    HeaderComponent
+  ], 
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
 export class App {
   title = 'GameLog';
   
-  // 1. Apenas declara a propriedade (adicionando o tipo Observable)
+  // 1. Apenas declara a propriedade
   isLoggedIn$!: Observable<boolean>; 
 
   constructor(private authService: AuthService) {
-    // 2. Inicializa a propriedade DENTRO do construtor, onde 'this.authService' existe
+    // 2. Inicializa a propriedade DENTRO do construtor
     this.isLoggedIn$ = this.authService.isLoggedIn$;
   }
 }
